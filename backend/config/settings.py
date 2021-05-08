bb953ed4bf2e073183f0c3a099992be8f16e9945
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-05-08 18:26:13 -0300

    Added djangorestframework package. Setted permission AllowAny for rest_framework explicitly

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+
     # local
     'todos',
 ]
@@ -127,3 +130,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST FRAMEWORK CONFIGURATIONS
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
