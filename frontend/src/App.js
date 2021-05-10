7896e8cb0576117bd89f7fe00caa4fcf32d6849f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-05-09 22:41:01 -0300

    Added axios module to integrate back-end and front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,33 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React, {Component} from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-
-const list = [
-  {
-    "id":1,
-    "title":"1st todo",
-    "body":"Learn Django properly."
-  },
-  {
-    "id":2,
-    "title":"Second Item",
-    "body":"Learn Python."
-  },
-  {
-    "id":3,
-    "title":"Learn HTTP",
-    "body":"It's important."
-  }
-]
+import axios from 'axios';
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {list};
+  state = {
+    todos:[]
+  };
+
+  componentDidMount(){
+    this.getTodos();
+  }
+
+  getTodos(){
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({todos:res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render(){
     return(
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
